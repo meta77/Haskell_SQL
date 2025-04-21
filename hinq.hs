a32472d54d8e15c95b8af049d470c4b01014a357
Meta77 <horokusa@keio.jp>
2025-04-21 20:04:17 +0900

    履修のモデルの定義

--- hinq.hs
+++ hinq.hs
@@ -283,7 +283,10 @@ maybeQuery1 :: HINQ (_select (teacherName . fst))
 
 
 
+-- 履修のモデル化
+data Enrollment = Enrollment { student :: Int, course :: Int } deriving Show
 
+-- 履修dataを使って、履修者のリストを作成
 
 
 
